4317672a1b279023d003ea09b53e366011d5db9c
Abhijay Bisht <768819@cognizant.com>
2024-01-10 15:25:56 +0530

    Adding Build

---
+++ build/static/js/289.b360855b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.b360855b.chunk.js.map
