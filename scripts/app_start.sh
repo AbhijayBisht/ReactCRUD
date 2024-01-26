9756d77a2e5ce47526e8ef8db4118b2239d03b81
Abhijay Bisht <768819@cognizant.com>
2024-01-26 21:37:51 +0530

    Updating Scripts

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "ReactCRUD" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+#npm start
+#pm2 start npm --name "ReactCRUD" -- start
+#pm2 startup
+#pm2 save
+#pm2 restart all
+
+systemctl -l enable nginx
+systemctl -l start nginx
 echo "Running app_starting steps..."
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /home/ec2-user/server
+
+apt-get install curl
 curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
-yum install -y nodejs
+#yum install -y nodejs
+apt-get install nodejs -y
+apt-get install npm -y
+apt install nginx -y
+ufw allow 'Nginx HTTP'
 echo "Running pre-installation steps..."
