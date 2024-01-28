627891a51fe6e14df31b1661de3e2b6d5b16f316
Abhijay Bisht <768819@cognizant.com>
2024-01-28 17:36:15 +0530

    updating after_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 cd /home/ec2-user/server
-npm install
-#npm install --save react react-dom react-scripts react-particles-js
-cp -r build/* /var/www/html
-npm install pm2 -g
+
 echo "Running post-installation steps..."
