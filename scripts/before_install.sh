8838efac5f517541eb607754d2a4323634a30aa7
Abhijay Bisht <768819@cognizant.com>
2024-01-28 19:51:14 +0530

    maybe correcting the fault

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
 apt-get install nodejs -y
 apt-get install npm -y
 apt install nginx -y
-ufw allow 'Nginx HTTP'
+#ufw allow 'Nginx HTTP'
 ufw allow 3000
 
 echo "Running pre-installation steps..."
