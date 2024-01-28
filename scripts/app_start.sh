40394952f5e2190da300857c5aeb542e78a2f9cd
Abhijay Bisht <768819@cognizant.com>
2024-01-28 19:42:37 +0530

    correcting the mistakes

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
 
-#pm2 start npm --name "ReactCRUD" -- start
-#pm2 startup
-#pm2 save
-#pm2 restart all
+#npm start
+npm install -g pm2
+pm2 start npm --name "ReactCRUD" -- start
+pm2 startup
+pm2 save
+pm2 restart all
 
 systemctl -l enable nginx
 systemctl -l start nginx
-npm start
+
 echo "Running app_starting steps..."
